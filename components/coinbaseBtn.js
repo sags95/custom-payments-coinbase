300a8b1b87cd95ac79c947e0b03a9e75957ec3b4
Alex Sagel <alex.sagel7587@gmail.com>
2023-05-31 13:09:07 -0400

    Minor style changes to coinbaseBtn loading

--- components/coinbaseBtn.js
+++ components/coinbaseBtn.js
@@ -11,6 +11,8 @@ export default function CoinbaseBtn({ cart }) {
 
     const [coinbaseCheckoutId, setCoinbaseCheckoutId] = useState(null)
     const [completeCheckout, paymentData, checkoutId, error] = useCoinbaseCheckout();
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(() => {
         const createCheckout = async () => {
@@ -21,7 +23,10 @@ export default function CoinbaseBtn({ cart }) {
             const data = res;
             return data.json();
         }
-        createCheckout().then((res) => setCoinbaseCheckoutId(res.data.id))
+        createCheckout().then((res) => {
+            setCoinbaseCheckoutId(res.data.id);
+            setIsLoading(false);
+        })
     }, [cart])
 
     useEffect(() => {
@@ -31,11 +36,25 @@ export default function CoinbaseBtn({ cart }) {
         }
     }, [paymentData, router, checkoutId]);
 
+    function Spinner() {
+        return (
+          <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        )
+      }
+      
 
 
-
-
-    if (coinbaseCheckoutId) {
+    if (isLoading) {
+        return (
+            <div className="flex items-center justify-center h-full">
+                <Spinner/>
+                <span>Loading...</span>
+            </div>
+        )
+    } else if (coinbaseCheckoutId) {
         return (
             <CoinbaseCommerceButton
                 checkoutId={coinbaseCheckoutId}
@@ -57,15 +76,6 @@ export default function CoinbaseBtn({ cart }) {
 
         )
     } else {
-        return (
-            <>
-       
-                <div className="block rounded dark:bg-sky-700 px-5 py-3 text-sm text-gray-100 transition dark:hover:bg-sky-700">
-                                          Buy With Crypto      
-                                            </div>
-          
-            </>
-           
-        )
+        return null;
     }
 }
